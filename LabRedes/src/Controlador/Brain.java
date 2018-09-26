ca9204fd4c35240a82122896fa598d1481b36333
IsaacMizrahi123 <43454414+IsaacMizrahi123@users.noreply.github.com>
2018-09-25 20:54:21 -0500

    Add files via upload
    Agregue comentarios para que se entendiera el proceso.

--- LabRedes/src/Controlador/Brain.java
+++ LabRedes/src/Controlador/Brain.java
@@ -24,21 +24,21 @@ public class Brain {
     }
 
     public static void GetInfo(File f) { //Se consigue los valores de numregistro, numcampos, ks y tipos
-        String line;
-        text = "";
+        String line;//Con la que se saca linea por linea  el texto del txt.
+        text = "";//Se "limpia" la variable donde todo el texto se va a contener.
         try {
             FileReader fr = new FileReader(f.getAbsolutePath());
             BufferedReader br = new BufferedReader(fr);
             line = br.readLine();
             while (line != null) {
-                text = text + line;
+                text = text + line;//Se agrega linea por linea del texto a la variable text. No se tiene ninguna separacion entre lineas.
                 line = br.readLine();
             }
             br.close();
             fr.close();
             System.out.println(text);//Esto es para comprobar
-            GetASCII(text);
-            GetHexAndBin(text);
+            GetASCII(text);//En esta funcion se le asigna a la variable "ASCIICode" la traduccion de todo "text" a ASCCI
+            GetHexAndBin(text);//En esta funcion 
         } catch (IOException e) {
             System.out.println("Error en GetInfo");
         }
@@ -60,25 +60,25 @@ public class Brain {
         System.out.println(HexCode);
     }
 
-    public static void GetHexAndBin(String characters) {
-        HexCode = "";
-        Bin = "";
-        String temp = "",as,bs;
+    public static void GetHexAndBin(String characters) {//Se va conviertiendo el texto leido a hexadecimal y enseguida a binario, para ahorrar tiempo.
+        HexCode = "";//Se "limpia" la variable HexCode donde se va a guardar la traduccion de "text" a su codigo hexadecimal.
+        Bin = "";//Se "limpia" la variable Bin donde se va a guardar la traduccion de los codigos Hexadecimales a Binario. Que es lo que se necesita enviar.
+        String temp = "",as,bs;//Variables necesarias para convertir y agregar caracteres a los Strings.
         int a,b;
         for (int i = 0; i < characters.length(); i++) {
-            temp = Integer.toHexString((int) characters.charAt(i));
-            HexCode = HexCode + temp;
-            a = Integer.parseInt(Character.toString(temp.charAt(0)),16);
-            b = Integer.parseInt(Character.toString(temp.charAt(1)),16);
-            as = Integer.toBinaryString(a);
-            bs = Integer.toBinaryString(b);
-            while(as.length()<4){
+            temp = Integer.toHexString((int) characters.charAt(i));//Se convierte letra por letra a Hexadecimal, que siempre son dos letras. Ej: Letra:k -> Hex:4b
+            HexCode = HexCode + temp;//Se le agrega al string que va a mantener todos los codigos hexadecimales en secuencia.
+            a = Integer.parseInt(Character.toString(temp.charAt(0)),16);//Se convierte el primer digito del numero hexadecimal a int para porder convertirlo a binario luego. Ej: String:4 -> Int:4
+            b = Integer.parseInt(Character.toString(temp.charAt(1)),16);//Se convierte el segundo digito a numero. Ej: String:B -> Int:11
+            as = Integer.toBinaryString(a);//Convierte el int a binario. Ej: Int: 4 -> Bin: 100
+            bs = Integer.toBinaryString(b);//Convierte el segundo int a binario. Ej: Int:11 -> Bin:1011
+            while(as.length()<4){//Se le agregan los ceros necesario para completar los 4 digitos. Ej: Bin:100 -> String:0100
                 as="0"+as;
             }
             while(bs.length()<4){
                 bs="0"+bs;
             }
-            Bin = Bin + as + bs;
+            Bin = Bin + as + bs;//Se guardan los codigos binarios. Cada caracter son 8 digitos binarios.
         }
         System.out.println(HexCode);//Para comprobar
         System.out.println(Bin);//Para comprobar
